699709ff45289b1caf7f409100fe2d610b1a164c
aliyevorkhan <orxanaliyev19@gmail.com>
2020-09-07 15:43:36 +0300

    added uploaded file field

--- article/models.py
+++ article/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 class Article(models.Model):
-    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE,verbose_name="Author")
+    title = models.CharField(max_length=50, verbose_name="Title")
+    content = RichTextField()
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Published date")
+    article_image = models.FileField(blank=True, null=True, verbose_name="Add image to article")
 
     def __str__(self):
         return self.title
