a6ea5a5cbf62d65ca81298a07230dcc8588bed4e
aliyevorkhan <orxanaliyev19@gmail.com>
2020-09-07 18:28:06 +0300

    registered Comment model

--- article/admin.py
+++ article/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article,Comment
 # Register your models here.
+
+admin.site.register(Comment)
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
