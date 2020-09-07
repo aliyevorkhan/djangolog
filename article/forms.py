83949c2fe09a533cc7316262dd726e2cc2e09d7f
aliyevorkhan <orxanaliyev19@gmail.com>
2020-09-07 15:42:51 +0300

    file upload field

--- article/forms.py
+++ article/forms.py
@@ -4,5 +4,5 @@ from .models import Article
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'content']
+        fields = ['title', 'content','article_image']
         
