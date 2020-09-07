23b4464883071dd45cec3e775170ca5646ceed24
aliyevorkhan <orxanaliyev19@gmail.com>
2020-09-07 15:42:06 +0300

    added features: login required, filed upload,  update_article(), delete_article(), articles() -> list all articles; changes:  redirect urls

--- article/views.py
+++ article/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from .forms import ArticleForm
 from .models import Article
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -10,6 +10,7 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required(login_url='user:login')
 def dashboard(request):
     articles = Article.objects.filter(author=request.user)
     context = {
@@ -17,15 +18,16 @@ def dashboard(request):
     }
     return render(request, 'dashboard.html',context)
 
+@login_required(login_url='user:login')
 def add_article(request):
-    form = ArticleForm(request.POST or None)
-
+    form = ArticleForm(request.POST or None,request.FILES or None)
+    print(form.files)
     if form.is_valid():
         article = form.save(commit=False)
         article.author = request.user
         article.save()
         messages.success(request, 'Your article was created successfully!')
-        return redirect('index') 
+        return redirect('article:dashboard') 
     context = {
         'form':form,
     }
@@ -38,3 +40,39 @@ def detail(request, id):
         'article':article,
     }
     return render(request, 'detail.html',context)
+
+@login_required(login_url='user:login')
+def update_article(request, id):
+    article = get_object_or_404(Article,id=id)
+    form = ArticleForm(request.POST or None, request.FILES or None, instance=article)
+
+    if form.is_valid():
+        article = form.save(commit=False)
+        article.author = request.user
+        article.save()
+        messages.success(request, 'Your article was created successfully!')
+        return redirect('article:dashboard') 
+    context = {
+        'form':form,
+    }
+    return render(request, 'update.html',context)
+
+@login_required(login_url='user:login')
+def delete_article(request,id):
+    article = get_object_or_404(Article,id=id)
+    article.delete()
+    messages.success(request, "Article deleted successfully")
+    return redirect('article:dashboard')
+
+def articles(request):
+    keyword = request.GET.get('keyword')
+
+    articles = Article.objects.all()
+
+    if keyword:
+        articles = Article.objects.filter(title__contains = keyword)
+
+    context = {
+        'articles':articles,
+    }
+    return render(request, 'articles.html', context)
