d97578d57c3ac758a96fdcc79da4aee479684883
aliyevorkhan <orxanaliyev19@gmail.com>
2020-09-07 15:47:11 +0300

    added and created new urls for specified methods

--- article/urls.py
+++ article/urls.py
@@ -8,5 +8,9 @@ app_name = "article"
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('add_article/', views.add_article, name='add_article'),
-    path('<int:id>/',views.detail, name='detail')
+    path('<int:id>/',views.detail, name='detail'),
+    path('update/<int:id>/', views.update_article, name='update'),
+    path('delete/<int:id>/', views.delete_article, name='delete'),
+    path('articles/', views.articles, name='articles'),
+
 ]
