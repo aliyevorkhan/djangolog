8b0d9e5a65437b74df5d9ae8c856e669a922455d
aliyevorkhan <orxanaliyev19@gmail.com>
2020-09-07 15:44:29 +0300

    added new urls for new features

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from article import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,5 @@ urlpatterns = [
     path('article/', include('article.urls')),
     path('user/', include('user.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
